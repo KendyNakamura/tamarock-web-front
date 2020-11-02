33dc9e1a589f5370e671a6c444c12b1773adf64c
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2020-11-02 21:39:50 +0900

    アナリティクス追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import "../styles/global.scss";
 import "../styles/global-sp.scss";
+import * as gtag from "../lib/gtag";
+import Router from "next/router";
+
+Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
