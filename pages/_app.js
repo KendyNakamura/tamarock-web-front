17449f840b58f056915e9495c1b6fe05f9f30a99
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2020-11-03 11:22:01 +0900

    本番環境のみアナリティクス

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import "../styles/global.scss";
 import "../styles/global-sp.scss";
 import * as gtag from "../lib/gtag";
-import Router from "next/router";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
+const App = ({ Component, pageProps }) => {
+  if (process.env.NODE_ENV !== "development") {
+    const router = useRouter();
+    useEffect(() => {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url);
+      };
+      router.events.on("routeChangeComplete", handleRouteChange);
+      return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+      };
+    }, [router.events]);
+  }
 
-export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
-}
+};
+
+export default App;
