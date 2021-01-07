a815b85e1d39e1db470f233a1a951422fee42fe3
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2021-01-07 12:46:43 +0900

    nextjsアップグレード

--- components/layout.js
+++ components/layout.js
@@ -24,9 +24,6 @@ export default function Layout({ children, home, headTitle, description, imageUr
         <meta name="description" content={siteDescription} />
         <meta
           property="og:image"
-          // content={`https://og-image.now.sh/${encodeURI(
-          //   siteTitle
-          // )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
           content={imageURL}
         />
         <meta name="og:title" content={siteTitle} />
