454f02c547be3e06ea2b5cda60ac8d6e7630b6a1
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2021-01-22 18:15:53 +0900

    ローカルアクセスを可能に

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   images: {
     domains: [
-      'tamarock-local.s3-ap-northeast-1.amazonaws.com',
-      'tamarock-prod.s3-ap-northeast-1.amazonaws.com',
-      'i.scdn.co',
+      "tamarock-local.s3-ap-northeast-1.amazonaws.com",
+      "tamarock-prod.s3-ap-northeast-1.amazonaws.com",
+      "i.scdn.co",
+      "localhost",
+      "tamarock-api",
     ],
   },
-}
+};
