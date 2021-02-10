24a92dbfb3f9cec152d1fca585acefc9186380c8
n.kenji <n.kenji@dayneeds.jp>
2021-02-10 10:21:27 +0900

    ssg index page

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Layout from "../components/layout";
 import ArticleList from "../components/article/articleList";
 import ArtistList from "../components/artist/artistList";
 import Link from "next/link";
-import React from "react";
-import fetch from "isomorphic-unfetch";
+import { getLimitedArticlesData } from "../lib/articles";
+import { getLimitedArtistsData } from "../lib/artists";
 
 export default function SingleLineGridList({ artistList, newsList, blogList }) {
   return (
@@ -33,19 +33,10 @@ export default function SingleLineGridList({ artistList, newsList, blogList }) {
   );
 }
 
-export async function getServerSideProps() {
-  const artistRes = await fetch(
-    `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/artist/infos?_end=10&_order=DESC&_sort=id&_start=0`
-  );
-  const artistList = await artistRes.json();
-  const news = await fetch(
-    `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/articles?_end=5&_order=DESC&_sort=id&_start=0&column=category&q=1`
-  );
-  const newsList = await news.json();
-  const blog = await fetch(
-    `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/articles?_end=5&_order=DESC&_sort=id&_start=0&column=category&q=2`
-  );
-  const blogList = await blog.json();
+export async function getStaticProps() {
+  const artistList = await getLimitedArtistsData();
+  const newsList = await getLimitedArticlesData(5, 1, 1);
+  const blogList = await getLimitedArticlesData(5, 1, 1);
   return {
     props: {
       artistList,
