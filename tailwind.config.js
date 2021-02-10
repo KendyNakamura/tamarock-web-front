4f30762598ae72d6752ac7ea984bdcd0c2e045c5
n.kenji <n.kenji@dayneeds.jp>
2021-02-10 17:18:16 +0900

    install tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  purge: [
-    // Use *.tsx if using TypeScript
-    "./pages/**/*.js",
-    "./components/**/*.js",
-  ],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 };
