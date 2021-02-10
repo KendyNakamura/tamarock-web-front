b6ffe7a5bc8145033bd48be0a735ea3f40a92746
n.kenji <n.kenji@dayneeds.jp>
2021-02-10 21:31:24 +0900

    create header

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "../styles/header.module.css";
@@ -8,6 +9,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 export default function PrimarySearchAppBar() {
   const [query, setQuery] = useState("");
   const [check, setCheck] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
 
   function falseCheck() {
@@ -23,12 +25,12 @@ export default function PrimarySearchAppBar() {
   }
 
   var href = {
-    pathname: query ? "/search": "/",
+    pathname: query ? "/search" : "/",
     query: query ? { name: encodeURI(query) } : "",
   };
 
   function handleKeyPress(e) {
-    console.log(query)
+    console.log(query);
     if (e.key == "Enter") {
       e.preventDefault();
       router.push(href);
@@ -37,61 +39,132 @@ export default function PrimarySearchAppBar() {
 
   return (
     <>
-      <header className={styles.header}>
-        <Link href="/">
-          <a>
-            <h1>たまロック</h1>
-          </a>
-        </Link>
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <Link href="/">
+                  <a>
+                    <img className="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+                  </a>
+                </Link>
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <Link href="/articles/page/1">
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">記事一覧</a>
+                  </Link>
+                  <Link href="/artists/page/1">
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">アーティスト一覧</a>
+                  </Link>
+                  <Link href="/about">
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">about</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
 
-        <nav className={styles.nav}>
-          <input
-            id="hamburger"
-            className="hamburger sp-menu"
-            type="checkbox"
-            onChange={(e) => {}}
-            checked={check}
-          />
-          <label
-            className="hamburger sp-menu"
-            htmlFor="hamburger"
-            onClick={handleCheck}
-          >
-            <i></i>
-          </label>
-          <section className="drawer-list">
-            <ul className={styles.ul}>
-              <li className={styles.li} onClick={falseCheck}>
-                <Link href="/articles">
-                  <a>記事一覧</a>
-                </Link>
-              </li>
-              <li className={styles.li} onClick={falseCheck}>
-                <Link href="/artists">
-                  <a>アーティスト一覧</a>
-                </Link>
-              </li>
-              <li className={styles.li} onClick={falseCheck}>
-                <Link href="/about">
-                  <a>about</a>
-                </Link>
-              </li>
-            </ul>
-          </section>
-        </nav>
-      </header>
+            {/* <div className="hidden md:block">
+            <div className="ml-4 flex items-center md:ml-6">
+              <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                <span className="sr-only">View notifications</span>
+                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                </svg>
+              </button>
+              <div className="ml-3 relative">
+                <div>
+                  <button onClick={() => setIsOpen(!isOpen)} className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
+                    <span className="sr-only">Open user menu</span>
+                    <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                  </button>
+                </div>
+
+                <Transition show={isOpen} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75 transform" leaveFrom="transition ease-in duration-75" leaveTo="transform opacity-0 scale-95">
+                  {(ref) => (
+                    <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+                        Your Profile
+                      </a>
+
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+                        Settings
+                      </a>
+
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+                        Sign out
+                      </a>
+                    </div>
+                  )}
+                </Transition>
+              </div>
+            </div>
+          </div> */}
+
+            <div className="-mr-2 flex md:hidden">
+              <button onClick={() => setIsOpen(!isOpen)} className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                <span className="sr-only">Open main menu</span>
+                <svg className={`${isOpen ? "hidden" : "block"} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                <svg className={`${isOpen ? "block" : "hidden"} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link href="/articles/page/1">
+              <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">記事一覧</a>
+            </Link>
+            <Link href="/artists/page/1">
+              <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">アーティスト一覧</a>
+            </Link>
+            <Link href="/about">
+              <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">about</a>
+            </Link>
+          </div>
+          {/* <div className="pt-4 pb-3 border-t border-gray-700">
+          <div className="flex items-center px-5">
+            <div className="flex-shrink-0">
+              <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+            </div>
+            <div className="ml-3">
+              <div className="text-base font-medium leading-none text-white">Tom Cook</div>
+              <div className="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
+            </div>
+            <button className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <span className="sr-only">View notifications</span>
+              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+              </svg>
+            </button>
+          </div>
+          <div className={`${isOpen ? "block" : "hidden"} mt-3 px-2 space-y-1`}>
+            <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">
+              Your Profile
+            </a>
+
+            <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">
+              Settings
+            </a>
+
+            <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">
+              Sign out
+            </a>
+          </div>
+        </div> */}
+        </div>
+      </nav>
       <div className="sFormWrapper">
         <form className="sForm">
-          <input
-            type="text"
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-            placeholder="アーティスト名を入力"
-            className="sBox"
-            onKeyPress={(e) => handleKeyPress(e)}
-          />
+          <input type="text" onChange={(e) => setQuery(e.target.value)} value={query} placeholder="アーティスト名を入力" className="sBox" onKeyPress={(e) => handleKeyPress(e)} />
           <Link href={href}>
-            <a onClick={falseCheck} className={`sLink ${ query ? "" : "noLink"}`}>
+            <a onClick={falseCheck} className={`sLink ${query ? "" : "noLink"}`}>
               <button className="sBtn">
                 <FontAwesomeIcon icon={faSearch} />
               </button>
