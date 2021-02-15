739c4e9e76b11b9faf7c97ad3d79489247033354
n.kenji <n.kenji@dayneeds.jp>
2021-02-15 15:51:19 +0900

    fix design

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -15,7 +15,7 @@ const Search: React.FC<SEARCHLISTS> = ({ listData }) => {
       <Box title="search">
         <ul className="list-none p-0">
           {listData.map(({ name, id }) => (
-            <li className="pl-5 my-8 border-b-2 border-yellow-400 border-dashed" key={id}>
+            <li className="pl-2 my-4 border-b-2 border-yellow-400 border-dashed" key={id}>
               <Link href="/artists/[id]" as={`/artists/${id}`}>
                 <a>{name}</a>
               </Link>
